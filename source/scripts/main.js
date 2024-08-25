df37118ea6308b125f8cd82b1f223510c0a064ce
IdealistCat <113909588+IdealistCat@users.noreply.github.com>
2024-08-25 20:55:30 +0000

    background + main.js setup

---
+++ source/scripts/main.js
@@ -0,0 +1,23 @@
+// objects
+let background = null;
+let floor = null;
+let player = null;
+let enemy = null;
+
+// game variables
+const FPS = 30;
+const gameInterval = setInterval(gameLoop, 1000 / FPS);
+
+// main functions
+function update() {
+
+}
+
+function draw() {
+}
+
+// loop
+const gameLoop = () => {
+    update();
+    draw();
+};
